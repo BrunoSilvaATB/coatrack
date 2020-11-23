750f3b05b876ec3e577539c1cfabf2a67ccdf3b8
BrunoSilvaATB <silva@atb-bremen.de>
2020-11-23 14:33:23 +0100

    Created script for windows systems of the build script (cmd file)

---
+++ build.cmd
@@ -0,0 +1,7 @@
+:: Script to build CoatRack using Maven in Windows systems
+
+:: Please note:
+:: - a CoatRack build requires some mail configuration parameters to be set
+:: - the configuration values in this file are just dummy/fallback values for development builds
+
+mvn clean install -e -Dygg.mail.sender.user="" -Dygg.mail.sender.password="" -Dygg.mail.server.url="" -Dygg.mail.server.port=0
